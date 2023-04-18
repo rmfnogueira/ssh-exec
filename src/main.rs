e4a37a8d8366da1edecdf87a2eb509c987b282a0
Rui Nogueira <ruinogueira@tuta.io>
2023-04-18 11:25:22 +0000

    added function to handle password input

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
-use async_ssh2_tokio::client::{Client, AuthMethod, ServerCheckMethod};
-use clap::Parser;
+use async_ssh2_tokio::client::{Client, AuthMethod, ServerCheckMethod, self};
+use clap::{Parser};
+use rpassword::read_password;
+
 
 #[derive(Debug, Parser)]
 struct Cli {
     /// should be the same for all hosts
     username: String,
     /// should be the same for all hosts
-    password: String,
-    /// command to be run on all hosts
     command: String,
     /// hosts socket addresses
     ipv4addr: Vec<String>,
 }
 
+fn prompt_password() -> Option<Result<String, async_ssh2_tokio::Error>> {
+    let password = read_password().ok()?;
+    if password.is_empty() {
+        None
+    } else {
+        Some(Ok(password))
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), async_ssh2_tokio::Error> {
-    let args = Cli::parse();
-
-    for ip in args.ipv4addr {
-        let client = Client::connect(
-            (ip, 22),
-            &args.password,
-            AuthMethod::with_password(&args.password),
-            ServerCheckMethod::NoCheck,
-        ).await?;
-        client.execute("ls").await?;
+    let password = match prompt_password() {
+        Some(Ok(p)) => p,
+        Some(Err(e)) => return Err(e),
+        None => {
+            // Handle the case where no password was provided
+            println!("No password provided");
+            return Ok(());
+        }
     };
-    Ok(())
-}
+
+    let args = Cli::parse();
+    for ip in &args.ipv4addr {
+            let client = Client::connect(
+                ip,
+                &password,
+                AuthMethod::with_password(&password),
+                ServerCheckMethod::NoCheck,
+            ).await?;
+            client.execute("ls").await?;
+        };
+        Ok(())
+    }
